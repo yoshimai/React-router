93c45f63730c1d21e21a7e823dddb1b8dee7ef67
yoshimai <117501051+yoshimai@users.noreply.github.com>
2022-11-14 17:13:30 +0900

    page1にoutletを追加

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 import { Home } from "./Home";
 import { Page1 } from "./Page1";
 import { Page2 } from "./Page2";
+import { Page1DetailA } from "./Page1DetailA";
+import { Page1DetailB } from "./Page1DetailB";
 import "./styles.css";
 
 export default function App() {
@@ -17,7 +19,10 @@ export default function App() {
       </div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/page1" element={<Page1 />} />
+        <Route path="/page1" element={<Page1 />}>
+          <Route path="detailA" element={<Page1DetailA />} />
+          <Route path="detailB" element={<Page1DetailB />} />
+        </Route>
         <Route path="/page2" element={<Page2 />} />
       </Routes>
     </BrowserRouter>
--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export const Page1 = () => {
   return (
@@ -7,6 +7,7 @@ export const Page1 = () => {
       <Link to="/page1/detailA">DetailA</Link>
       <br />
       <Link to="/page1/detailB">DetailB</Link>
+      <Outlet />
     </div>
   );
 };
