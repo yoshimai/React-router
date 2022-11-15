50757e0720f4247c162b60d0fe1d3365b130684a
yoshimai <117501051+yoshimai@users.noreply.github.com>
2022-11-15 09:53:15 +0900

    DetailにLinkついか

--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 export const Page1DetailA = () => {
   return (
     <div>
       <h1>Page1DetailAページです</h1>
+      <Link to="/page1/detailB">DetailB</Link>
     </div>
   );
 };
--- src/Page1DetailB.jsx
+++ src/Page1DetailB.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 export const Page1DetailB = () => {
   return (
     <div>
       <h1>Page1DetailBページです</h1>
+      <Link to="/page1/detailA">DetailA</Link>
     </div>
   );
 };
